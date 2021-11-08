91c89b114b25151e79c6f9d83a1ccd91f2ebf4c7
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2021-11-08 12:57:51 +0700

    Update Category_Management.php

--- Category_Management.php
+++ Category_Management.php
@@ -1,12 +1,15 @@
-<?php
-if(!isset($_SESSION['admin']) or $_SESSION['admin']==0)
-{
-    echo"<script>alert('You are not administration')</script>";
-    echo'<meta http-equiv="refresh" content="0;URL=index.php">';
-}
-else
-{
-?>
+    <?php
+    if(!isset($_SESSION['admin']) or $_SESSION['admin']==0)
+    {
+        echo"<script>alert('You are not administration')</script>";
+        echo'<meta http-equiv="refresh" content="0;URL=index.php">';
+    }
+    else
+    {
+    ?>
+    <!-- Bootstrap --> 
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+	<meta charset="utf-8" />
 	<link rel="stylesheet" href="css/bootstrap.min.css">
     <script language="javascript">
     function deleteConfirm(){
@@ -30,7 +33,7 @@ else
         <form name="frm" method="post" action="">
         <h1>Product Category</h1>
         <p>
-        <img src="image/add.png" alt="Add new" width="16" height="16" border="0" /> <a href="?page=add_category"> Add</a>
+        <img src="images/add.png" alt="Add new" width="16" height="16" border="0" /> <a href="?page=add_category"> Add</a>
         </p>
         <table id="tablecategory" class="table table-striped table-bordered" cellspacing="0" width="100%">
             <thead>
@@ -47,18 +50,18 @@ else
             <?php
             $No=1;
             $result=pg_query($conn,"SELECT * FROM category");
-            while($row=pg_fetch_array($result, Null, PGSQL_ASSOC))
+            while($row=pg_fetch_array($result,Null, PGSQL_ASSOC))
             {
             ?>
 			<tr>
-              <td class="CheckBox"><?php echo $No;?></td>
+              <td class="cotCheckBox"><?php echo $No;?></td>
               <td><?php echo $row["cat_name"];?></td>
               <td><?php echo $row["cat_des"];?></td>
               
-              <td style='text-align:center'><a href="?page=update_category&&id=<?php echo $row["cat_id"];?>"><img src='image/edit.png' border='0'/></a></td>
+              <td style='text-align:center'><a href="?page=update_category&&id=<?php echo $row["cat_id"];?>"><img src='images/edit.png' border='0'/></a></td>
               <td style='text-align:center'>
               <a href="?page=management_category&&function=del&&id=<?php echo $row["cat_id"];?>" onclick="return deleteConfirm()">
-              <img src='image/delete.png' border='0' /></a></td>
+              <img src='images/delete.png' border='0' /></a></td>
             </tr>
             <?php 
             $No++;
@@ -70,11 +73,10 @@ else
         
         <!--Nút Thêm mới , xóa tất cả-->
         <div class="row" style="background-color:#FFF"><!--Nút chức nang-->
-            <div class="col-md-12">
-            	
-            </div>
+            <div class="col-md-12"></div>
         </div><!--Nút chức nang-->
  </form>
 <?php
-}
+    }
 ?>
+   
